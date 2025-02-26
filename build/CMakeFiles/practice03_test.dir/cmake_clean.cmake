5bc6b0e75ab0012218fa2d3712496d945538a2e3
stefanyromannn <romanstefany3@gmail.com>
2025-02-26 06:48:32 +0000

    new

---
+++ build/CMakeFiles/practice03_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/practice03_test.dir/practice/practice03/practice03.cpp.o"
+  "CMakeFiles/practice03_test.dir/tests/practice03_test/practice03_test.cpp.o"
+  "practice03_test"
+  "practice03_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/practice03_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
